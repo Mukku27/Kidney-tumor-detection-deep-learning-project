b5fe06b763d80bc85239b174a6f9f0bc2ba37847
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2024-05-21 14:15:49 +0530

    setup complete

--- setup.py
+++ setup.py
@@ -1,23 +1,41 @@
 import setuptools
 
-with open("README.md","r",encoding='utf-8') as f:
-    long_description=f.read()
+# Readme file
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
 
-__version__='0.0.0'
+# Requirements file
+with open("requirements.txt", "r", encoding='utf-8') as f:
+    requirements = f.read().splitlines()
 
-REPO_NAME='Kidney-tumor-detection-deep-learning-project'
+# Metadata
+__version__ = '0.0.0'
+REPO_NAME = 'Kidney-tumor-detection-deep-learning-project'
+AUTHOR_USER_NAME = 'VemulapalliMukesh27'
+SRC_REPO = 'cnnClassifier'
+AUTHOR_EMAIL = 'vemulapallimukesh@gmail.com'
 
-AUTHOR_USER_NAME='VemulapalliMukesh27'
-SRC_REPO='cnnClassifier'
-AUTHOR_EMAIL='vemulapallimukesh@gmail.com'
-
+# Setup config
 setuptools.setup(
-   name=SRC_REPO
-   version=__version__
-   author=AUTHOR_USER_NAME
-   description='A PYTHON PACKAGE FOR CNN'
-   long_description=long_description
-   
-
-
+    name=SRC_REPO,  # Package name
+    version=__version__,  # Version
+    author=AUTHOR_USER_NAME,  # Author name
+    author_email=AUTHOR_EMAIL,  # Author email
+    description='A PYTHON PACKAGE FOR CNN',  # Short description
+    long_description=long_description,  # Long description
+    long_description_content_type="text/markdown",  # Description type
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",  # Project URL
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",  # Issues URL
+    },
+    package_dir={"": "src"},  # Package dir
+    packages=setuptools.find_packages(where="src"),  # Find packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],  # Classifiers
+    python_requires='>=3.6',  # Python version
+    install_requires=requirements,  # Dependencies
+    license='MIT',  # License
 )
